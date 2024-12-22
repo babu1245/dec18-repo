71496870af77b52d7776392eb18694303d3b49e0
babu1245 <164604560+babu1245@users.noreply.github.com>
2024-12-22 16:50:41 +0530

    sadfads

---
+++ terraform.tfstate.tf
@@ -0,0 +1,9 @@
+{
+  "version": 4,
+  "terraform_version": "1.10.3",
+  "serial": 1,
+  "lineage": "e8f5686c-4410-7e28-cb7d-8a300deda21b",
+  "outputs": {},
+  "resources": [],
+  "check_results": null
+}
